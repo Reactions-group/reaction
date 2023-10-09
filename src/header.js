0fc0b049e8f365a17b1d8fe92b021f44b301afca
MarahAlasmar <alasmarmarah567@gmail.com>
2023-10-09 14:43:30 +0300

    update2

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Image from 'react-bootstrap/Image';
-import logoImage from './11.png'
+import logoImage from './12.png'
 import './navstyle.css'; 
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './Login';
@@ -23,10 +23,10 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home"><b>Home</b></Nav.Link>
+            <Nav.Link href="/"><b>Home</b></Nav.Link>
             <Nav.Link href="#about"><b>About Us</b></Nav.Link>
             <br/>
-                <Navbar.Brand className="mx-auto" href="#home">
+            <Navbar.Brand className="mx-auto" href="/">
   <Image src={logoImage} alt="Logo" height="60" loading="lazy" />
 </Navbar.Brand>
             <Nav.Link href="#contact"><b>Contact Us</b></Nav.Link>
